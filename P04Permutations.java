ef840f535bc3f3c54951b5f7f1ac7514a0d1f05c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2019-06-05 14:03:56 -0400

    Update P04Permutations.java
    Broke up the permute() method a bit. Renamed the shouldMergeDuplicates flag to allowDuplicates.

--- P04Permutations.java
+++ P04Permutations.java
@@ -14,7 +14,7 @@ public class P04Permutations {
 		final Scanner CIN; /* standard input scanner */
 
 		final String INPUT; /* user's input */
-		final boolean SHOULD_MERGE; /* whether duplicates should be merged */
+		final boolean SHOULD_DUPLICATE; /* whether duplicates should be displayed */
 
 		/* Printing functor -- prints out a given character sequence */
 		final Function <CharSequence,Void> PRINT = ((cs) -> {
@@ -24,15 +24,18 @@ public class P04Permutations {
 
 		CIN = new Scanner(System.in); /* creates a scanner on standard input */
 
-		/* */
+		/* ask for the string to permute */
 		System.err.printf("Type in a string to permute.\n> ");
 		INPUT = CIN.nextLine();
 
-		System.err.printf("Should duplicates be merged? [yN] ");
-		SHOULD_MERGE = ("Y".equals(CIN.nextLine().toUpperCase()));
+		/* ask whether to display duplicates
+		 * (anything other than 'N' or 'n' is yes) */
+		System.err.printf("Allow duplicates? [Yn] ");
+		SHOULD_DUPLICATE = (!("N".equals(CIN.nextLine().toUpperCase())));
 
-		permute(INPUT, 0, INPUT.length(), PRINT, SHOULD_MERGE);
-	}
+		/* permute the input */
+		permute(INPUT, 0, INPUT.length(), PRINT, SHOULD_DUPLICATE);
+	} /* end public static void main(String... argv) */
 
 
 	/******************************************************************//**
@@ -40,55 +43,97 @@ public class P04Permutations {
 	 * specified by [off, end[, then performs the specified function on
 	 * each permutation.
 	 *
-	 * @param shouldMergeDuplicates -- if true, all duplicates will be
-	 *   merged; otherwise, duplicates will appear in the permutations
+	 * @param allowDuplicates -- if true, duplicates will appear
+	 *   in the permutations; otherwise, they will not
 	 */
 	public static void permute(
 			final CharSequence cs, final int off, final int end,
 			final Function<CharSequence,?> function,
-			final boolean shouldMergeDuplicates
+			final boolean allowDuplicates
 	) {
-		StringBuilder permutation; /* stores new permutations */
-		boolean should_swap; /* whether the current iteration should
-		                      * be swapped */
-
 		/* bc: if done swapping, call the function and return */
 		if (off == end) {
 			function.call(cs);
 			return;
 		} /* end if (off == end) */
 
-		/* rc:.. */
+		/* rc: perform permutations */
+		/* permutations without the current character swapped */
+		permute(cs, (off + 1), end, function, allowDuplicates);
+		/* permutations with the current character swapped with each
+		 * following character */
+		permuteSwapWithFollowing(cs, off, end, function, allowDuplicates);
+	} /* end static void permute(
+	   * 	final CharSequence cs, final int off, final int end,
+	   * 	final Function<CharSequence,?> function,
+	   * 	final boolean allowDuplicates
+	   * )
+	   */
 
-		/* permute the current permutation after the current offset */
-		permute(cs, (off + 1), end, function, shouldMergeDuplicates);
+
+	/******************************************************************//**
+	 * Swaps the current character with each of the characters
+	 * following it, then performs permutation after the current character.
+	 *
+	 * @param allowDuplicates -- if true, duplicates will appear
+	 *   in the permutations; otherwise, they will not
+	 */
+	public static void permuteSwapWithFollowing(
+			final CharSequence cs, final int off, final int end,
+			final Function<CharSequence,?> function,
+			final boolean allowDuplicates
+	) {
+		StringBuilder permutation; /* stores new permutations */
+		boolean was_swapped; /* whether each current character has
+		                      * already been swapped */
 
 		/* loop through all following characters */
 		for (int k = (off + 1); (k < end); ++k) {
-			/* by default all characters should be swapped */
-			should_swap = true;
-			/* on if shouldMergeDuplicates flag is set */
-			if (shouldMergeDuplicates) {
-				/* search for the current character in the previous
-				 * characters swapped */
-				for (int l = off; (should_swap && (l < k)); ++l) {
-					if (cs.charAt(k) == cs.charAt(l)) {
-						should_swap = false;
-					} /* end if (cs.charAt(k) == cs.charAt(l)) */
-				} /* end for (int l = off; (should_swap && (l < k)); ++l) */
-			} /* end if (shouldMergeDuplicates) */
-
+			/* search for the current character in the previous
+			 * characters swapped.
+			 * the character is always counted as swapped if duplicates
+			 * are allowed */
+			was_swapped =
+				(allowDuplicates || !contains(cs, off, k, cs.charAt(k)));
 			/* if the current character was not previously swapped */
-			if (should_swap) {
+			if (was_swapped) {
 				/* copy the character sequence */
 				permutation = new StringBuilder(cs); 
 				/* swap with the original offset character */
 				swap(permutation, off, k);
 				/* permute after the current offset*/
-				permute(permutation, (off + 1), end, function, shouldMergeDuplicates);
-			} /* if (should_swap) */
+				permute(permutation, (off + 1), end, function, allowDuplicates);
+			} /* if (was_swapped) */
 		} /* end for (int k = (off + 1); (k < end); ++k) */
-	} /* end */
+	} /* end static void permuteSwapWithFollowing(
+	   * 	final CharSequence cs, final int off, final int end,
+	   * 	final Function<CharSequence,?> function,
+	   * 	final boolean allowDuplicates
+	   * )
+	   */
+
+
+	/******************************************************************//**
+	 * Searches the specified character sequence within the range
+	 * [off, end[ for the given key.
+	 * @return true if found; false otherwise
+	 */
+	public static boolean contains(
+		CharSequence cs, int off, int end, char key
+	) {
+		/* by default all characters should be swapped */
+		boolean is_found = false;
+		/* search for the current character in the previous
+		 * characters swapped */
+		for (int l = off; ((!is_found) && (l < end)); ++l) {
+			if (key == cs.charAt(l)) {
+				is_found = true;
+			} /* end if (key == cs.charAt(l)) */
+		} /* end for (int l = off; (is_found && (l < k)); ++l) */
+		return is_found;
+	} /* end static boolean contains(
+	   * 	CharSequence cs, int off, int end, char key
+	   * )*/
 
 
 	/******************************************************************//**
